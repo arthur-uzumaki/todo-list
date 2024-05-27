b2812571e8cc040d725052606be646fa21488e8d
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-05-27 12:51:40 -0300

    feat:(back) criando config do test

---
+++ server/vitest.config.ts
@@ -0,0 +1,17 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
+})
