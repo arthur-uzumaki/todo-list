134b64145880fb9390fab6218413325cf95b1bff
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-05-31 13:25:13 -0300

    test: criação do teste do use-case  get-task

---
+++ server/src/Domain/application/use-cases/get-task.spec.ts
@@ -0,0 +1,31 @@
+import { InMemoryTask } from '@/repositories/in-memory-task'
+import { GetTask } from '@/Domain/application/use-cases/get-task'
+import { makeTask } from '@/factoreis/make-task'
+
+describe('get task', () => {
+  let inMemory: InMemoryTask
+  let getTask: GetTask
+
+  beforeEach(() => {
+    inMemory = new InMemoryTask()
+    getTask = new GetTask(inMemory)
+  })
+
+  it('should be able get tasks', async () => {
+    const task1 = makeTask()
+
+    await inMemory.create(task1)
+
+    const { tasks } = await getTask.execute()
+
+    expect(tasks).toHaveLength(1)
+    expect(tasks).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          title: 'Gravação de aula',
+          startAt: new Date('03/11/2024'),
+        }),
+      ]),
+    )
+  })
+})
