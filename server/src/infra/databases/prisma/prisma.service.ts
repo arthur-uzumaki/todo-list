d64a29205130da6972d3d8b2159945c7b10c093b
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-06-02 22:49:36 -0300

    feat:(back) criando uma pasta infra, fazendo conexão com prisma

---
+++ server/src/infra/databases/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  constructor() {
+    super({
+      log: ['query', 'error'],
+    })
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
+  }
+}
