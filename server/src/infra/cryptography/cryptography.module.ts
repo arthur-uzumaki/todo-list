3ffe2a9a0bc4755e084031195815b3141e803e25
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-06-02 23:11:19 -0300

    feat:(back) criando module de criptografia

---
+++ server/src/infra/cryptography/cryptography.module.ts
@@ -0,0 +1,16 @@
+import { HashCompare } from '@/Domain/application/cryptography/hash-comparer'
+import { Module } from '@nestjs/common'
+import { CryptographyHasher } from '@/infra/cryptography/cryptography-hasher'
+import { HashGenerator } from '@/Domain/application/cryptography/hash-generator'
+import { Encrypter } from '@/Domain/application/cryptography/encrypter'
+import { JwtEncrypter } from '@/infra/cryptography/jwt-encrypter'
+
+@Module({
+  providers: [
+    { provide: HashCompare, useClass: CryptographyHasher },
+    { provide: HashGenerator, useClass: CryptographyHasher },
+    { provide: Encrypter, useClass: JwtEncrypter },
+  ],
+  exports: [HashCompare, HashGenerator, Encrypter],
+})
+export class CryptographyModule {}
