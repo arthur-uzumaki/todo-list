2aef412f665a2bf1ea724ad16e78bb8ed94b11ee
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-06-03 13:10:02 -0300

    test:(back) configuração do teste e2e

---
+++ server/src/infra/env/env.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
+})
+
+export type Env = z.infer<typeof envSchema>
---
+++ server/test/setup-e2e.ts
@@ -0,0 +1,39 @@
+import { envSchema } from '@/infra/env/env'
+import { PrismaClient } from '@prisma/client'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+const env = envSchema.parse(process.env)
+
+const prismaClient = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(env.DATABASE_URL)
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+  try {
+    const databaseUrl = generateUniqueDatabaseURL(schemaId)
+    process.env.DATABASE_URL = databaseUrl
+    execSync('pnpm prisma migrate deploy')
+  } catch (error) {
+    console.log('Error during database migration', error)
+    throw error
+  }
+})
+
+afterAll(async () => {
+  await prismaClient.$executeRawUnsafe(
+    `DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`,
+  )
+  await prismaClient.$disconnect()
+})
