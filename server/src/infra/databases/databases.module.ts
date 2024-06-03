f36563fe824cccc930186633daf912683d205337
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-06-02 23:39:15 -0300

    feat:(back) criando o repository de user

--- server/src/infra/databases/databases.module.ts
+++ server/src/infra/databases/databases.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from '@/infra/databases/prisma/prisma.service'
+import { UserRepository } from '@/Domain/application/repositories/user-repository'
+import { UserRepositoryPrisma } from '@/infra/databases/prisma/repositories/user-repository-prisma'
 
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UserRepository,
+      useClass: UserRepositoryPrisma,
+    },
+  ],
+  exports: [UserRepository],
 })
 export class DatabasesModule {}
