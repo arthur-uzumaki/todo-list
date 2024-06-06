81c120aec1576223a821940aedf6883e3f263702
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-06-06 11:16:18 -0300

    feat:(back) criando a rota pra obeter todas tasks

--- server/src/infra/http/controller/controller.module.ts
+++ server/src/infra/http/controller/controller.module.ts
@@ -4,6 +4,7 @@ import { RegisterModule } from '@/infra/http/controller/register/register.module
 import { CryptographyModule } from '@/infra/cryptography/cryptography.module'
 import { AuthUserModule } from '@/infra/http/controller/auth-user/auth-user.module'
 import { CreateTaskModule } from '@/infra/http/controller/create-task/create-task.module'
+import { FetchTasksModule } from '@/infra/http/controller/fetch-tasks/fetch-tasks.module'
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { CreateTaskModule } from '@/infra/http/controller/create-task/create-tas
     CryptographyModule,
     AuthUserModule,
     CreateTaskModule,
+    FetchTasksModule,
   ],
 })
 export class ControllerModule {}
