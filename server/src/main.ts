7dd5857ac84d3f677db1decf5fc6617c97b16e33
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-06-08 11:16:26 -0300

    feat:(back) criando a config do cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './infra/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  })
+
   await app.listen(3333)
 }
 bootstrap()
